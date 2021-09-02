1dfb80c2525f7e0db851e3dcde492d730180bfe1
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2021-09-03 00:58:05 +0530

    Create arrays.js

---
+++ w1d4Arrays/arrays.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
+*/ 
+module.exports = {maxOfThree, sum, multiply }; //add all of your function names here that you need for the node mocha tests
+
+/**
+ * 
+ * @param {number} a is a number
+ * @param {number} b is a number 
+ * @param {number} c is a number
+ * @returns {number} largest of a, b, c
+ */
+function maxOfThree(a, b, c){ 
+    return c;
+}
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function sum(arr){
+    let tot = 0;
+    for (const number of arr){
+        tot += number;
+    }
+    return tot;
+}
+
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function multiply(arr){
+    let tot = 1;
+
+    return tot;
+
+}
