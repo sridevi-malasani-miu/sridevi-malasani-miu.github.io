29a4d7330250fcc60c291dd6449fd862944b137c
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2021-09-07 02:05:45 +0530

    Create app.js

---
+++ w2d1objects/app.js
@@ -0,0 +1,49 @@
+"use strict";
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+module.exports = {findTitles }; //add all of your function names here that you need for the node mocha tests
+
+
+let library = [
+    { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
+    { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
+    { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3245 }
+];
+
+/**
+ * Event handler to display library titles sorted alphabetically
+ * @returns {undefined}
+ */
+function showTitles() {
+
+    /* put all titles into an array, then sort, then join with newline and insert in textarea innerHTML */
+
+    const titles = findTitles();
+
+    /*need to sort and then join the titles still (e.g., someArray.join("\n")  */
+    titles.sort();
+    const titleString = titles.join("\n");
+
+    let textArea = document.getElementById("displayArea");
+    textArea.innerHTML = titleString;
+}
+
+/**
+ * 
+ * @return {object} array holding all titles as elements
+ */
+function findTitles() {
+    let titles = [];
+    titles = ["Mockingjay: The Final Book of The Hunger Games", "The Road Ahead", "Walter Isaacson"];  //FIX THIS!!
+    // implement this and other functions
+    return titles;
+}
+
+/**
+ * @returns {undefined} no return
+ * Event handler for Add book button.  Creates and adds book to the library
+ */
+function addBook(title,author,libraryID){
+   // const title = document.getElementById("title"); //retrieves the book title from the title textbox
+    //finish the implementation -- get the author, create a book object, and add to the library array
+}
+© 2021 GitHub, Inc.
