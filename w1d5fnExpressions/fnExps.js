83155a552729a8522807df0e6d763a3c67292333
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2021-09-06 23:21:37 +0530

    Create fnExps.js

---
+++ w1d5fnExpressions/fnExps.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
+*/ 
+module.exports = {double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
+
+/**
+ *@returns {number} double the input
+ */
+function double(){
+}
+
+
+/**
+ * @returns {number} 100 times the input
+ */
+ function times100(){
+ }
+
+
+/**
+ * @returns {Array} creates a new array with function mapped to each element
+ */
+function myMap(){
+}
