9200d6a6c10d17b9311fcd4d895b597b2baf1a89
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2021-09-15 23:55:22 +0530

    Update d31node.js

--- w3d12Recursion/d31node.js
+++ w3d12Recursion/d31node.js
@@ -1,3 +1,29 @@
+let node3 = {
+ name: "p",
+ value: "This is text in the a paragraph",
+ children: null
+};
+let node4 = {
+ name: "label",
+ value: "Name", 
+ children: null
+};
+let node5 = {
+ name: "input",
+ value: "this was typed by a user",
+ children: null
+};
+let node2 = {
+ name: "div",
+ value: null,
+ children: [node4, node5]
+};
+let node1 = {
+ name: "body",
+ children: [node2, node3],
+ value: null,
+}
+
 function printNames(node1){
     consol.log(node1.name+":"+node1.value);
     if(node1.children && node1.children.length>0){
