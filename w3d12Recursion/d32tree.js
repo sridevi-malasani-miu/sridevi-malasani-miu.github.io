20ddec3781aecd8fb73413a8268875961cf8a081
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2021-09-16 02:28:42 +0530

    Update d32tree.js

--- w3d12Recursion/d32tree.js
+++ w3d12Recursion/d32tree.js
@@ -1,3 +1,4 @@
+module.exports = {getMatchingTree}
 function TreeNode(value) {
     this.value = value;
     this.descendents = [];
@@ -17,11 +18,39 @@ function contains(tree,name){
         return true;
     if(tree.descendents && tree.descendents.length>0){
         for(let item of tree.descendents){
-            console.log(item)
-            if(contains(item,name))
-                return true;
+           if(contains(item,name))
+               return true;
         }
     }
-    return false;
+   return false;
 }
+
+console.log( contains(abe,"Lisa"));
+function getMatchingTree(tree,name){
+    if(tree.value===name){
+        return tree;
+    }
+    if(tree.descendents && tree.descendents.length>0){
+        for(let item of tree.descendents){
+           let matchingNode = getMatchingTree(item,name);
+           if(matchingNode){
+                return matchingNode; 
+           }
+        }
+    }
+    return null;
+}
+console.log("getMatchingTree ",getMatchingTree(abe,"Lisa"));
+// Create a new constructor function ListNode (based on TreeNode below) and use it to generate a 
+// linked list of Abe, Homer, Bart, Lisa, Maggie instead of a tree.
+
+function ListNode(value,next){
+    this.value = value;
+}
+
+let maggieNode = new ListNode("Maggie",null);
+let lisaNode = new ListNode("Lisa",maggieNode);
+let bartNode = new ListNode("Bart",lisaNode);
+let homerNode = new ListNode("Homer",bartNode);
+let abeNode =  new ListNode("Abe",homerNode);
 
