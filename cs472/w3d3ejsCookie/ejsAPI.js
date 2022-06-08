31821b1fd0596258dd1791d9218e493485dce59e
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2022-06-08 13:41:20 -0500

    Create ejsAPI.js

---
+++ cs472/w3d3ejsCookie/ejsAPI.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cookieParse = require("cookie-parser");
+var path = require("path");
+var ejs = require("ejs");
+
+app.listen(80, () => {
+    console.log('Your Server is running on 3000');
+})
+
+app.use(cookieParse());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//EJS Engine
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+// app.set('views', path.join(__dirname,"myDir"));
+
+app.get("/admin", function (req, res, next) {
+    res.render("myFile", { list: ["apple", "banana"] })
+});
