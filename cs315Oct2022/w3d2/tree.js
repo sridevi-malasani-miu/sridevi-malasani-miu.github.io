8f5eb1072d635c4a639e409f40ce813fe6a08181
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2022-10-18 15:05:11 -0500

    Create tree.js

---
+++ cs315Oct2022/w3d2/tree.js
@@ -0,0 +1,35 @@
+let node3 = {
+    name: "p",
+    value: "This is text in the a paragraph",
+    children: null
+   };
+   let node4 = {
+    name: "label",
+    value: "Name",
+    children: null
+   };
+   let node5 = {
+    name: "input",
+    value: "this was typed by a user",
+    children: null
+   };
+   let node2 = {
+    name: "div",
+    value: null,
+    children: [node4, node5]
+   };
+   let node1 = {
+    name: "body",
+    children: [node2, node3],
+    value: null,
+   };
+
+   function print(parent){
+        console.log(parent.name+":"+parent.value);
+        if(!parent.children)
+            return;
+        for(let child of parent.children)
+            print(child)
+   }
+   
+   print(node1);
