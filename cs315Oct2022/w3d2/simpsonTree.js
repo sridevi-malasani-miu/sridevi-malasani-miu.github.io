2484751b6c6fcfc8a2b2c90deb03907401a3e10c
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2022-10-19 13:50:37 -0500

    Create simpsonTree.js

---
+++ cs315Oct2022/w3d2/simpsonTree.js
@@ -0,0 +1,56 @@
+function TreeNode(value) {
+    this.value = value;
+    this.descendents = [];
+}
+// create nodes with values
+const abe = new TreeNode('Abe');
+const homer = new TreeNode('Homer');
+const bart = new TreeNode('Bart');
+const lisa = new TreeNode('Lisa');
+const maggie = new TreeNode('Maggie');
+// associate root with is descendents
+abe.descendents.push(homer);
+homer.descendents.push(bart, lisa, maggie);
+
+function contains(abe, nodeName) {
+    if (abe.value == nodeName)
+        return true;
+    for (let child of abe.descendents) {
+        let result = contains(child, nodeName);
+        if (result)
+            return true;
+    }
+    return false;
+}
+console.log(contains(abe, 'Lisa'))
+
+function countLeafNodes(abe) {
+    if (abe.descendents.length == 0)
+        return 1;
+    let count = 0;
+    for (let child of abe.descendents) {
+        count = count + countLeafNodes(child);
+    }
+    return count;
+}
+//Abe, count = 0
+//homer, count =0, lisa,bart,maggie,
+// console.log(countLeafNodes(abe,'Abe'))
+
+function countParentNodes(abe) {
+    let count = 0;
+    for (let child of abe.descendents) {
+        count = 1 + countParentNodes(child);
+    }
+    return count;
+}
+console.log(countParentNodes(abe));
+
+function flattenTree(abe){
+    let names = [abe.value];
+    for(let child of abe.descendents){
+        names = names.concat(flattenTree(child));
+    }
+    return names;
+}
+console.log(flattenTree(abe))
