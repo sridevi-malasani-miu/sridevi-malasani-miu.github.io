198d6297d5a40785b0546c3efdfe7e03ed941ed7
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2022-12-15 14:30:26 -0600

    Create expressServer.js

---
+++ cs415/project/expressServer.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/myFile', function (req, res) {
+    res.sendFile( __dirname + "/public/" + "inventory.html" );
+ })
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(80, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
