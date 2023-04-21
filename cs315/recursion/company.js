c271ad53be278b650d8f3a3019502846525f5815
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2023-04-20 22:08:18 -0500

    Update company.js

--- cs315/recursion/company.js
+++ cs315/recursion/company.js
@@ -29,4 +29,64 @@ function sumSalariesByDept(company){
     }
     return deptSalary;
 }
-console.log( sumSalariesByDept(company));
+// console.log( sumSalariesByDept(company));
+
+// Get the avg salaries by department
+function avgSalariesByDept(company){
+    // console.log(company)
+   let deptSalary = {};
+    for(let deptName in company){
+        let dept = company[deptName];
+        let sum = 0;
+        if(Array.isArray(dept)){
+            sum+=dept.reduce(function(prev,emp){
+                return prev+emp.salary;
+            },0);
+            deptSalary[deptName]=sum/dept.length;
+        }else{
+            // console.log(deptSalary);
+            deptSalary = Object.assign(avgSalariesByDept(dept),deptSalary);
+        }
+    }
+    return deptSalary;
+}
+// console.log( avgSalariesByDept(company));
+
+// Get the max salaries by each department
+function maxSalariesByDept(company){
+   let deptSalary = {};
+    for(let deptName in company){
+        let dept = company[deptName];
+        
+        if(Array.isArray(dept)){
+            let max =dept.reduce(function(prev,emp){
+                return prev>emp.salary?prev:emp.salary;
+            },dept[0].salary);
+            deptSalary[deptName]=max;
+        }else{
+            // console.log(deptSalary);
+            deptSalary = Object.assign(maxSalariesByDept(dept),deptSalary);
+        }
+    }
+    return deptSalary;
+}
+// console.log( maxSalariesByDept(company));
+
+// Get the max salaries in Company
+function getHighestSalary(company){
+    let maxSalary =0;
+     for(let deptName in company){
+         let dept = company[deptName];
+         if(Array.isArray(dept)){
+             let deptMax =dept.reduce(function(prev,emp){
+                 return prev>emp.salary?prev:emp.salary;
+             },dept[0].salary);
+             maxSalary = maxSalary>deptMax?maxSalary:deptMax;
+         }else{
+             let temp = getHighestSalary(dept);
+             maxSalary = maxSalary>temp?maxSalary:temp;
+         }
+     }
+     return maxSalary;
+ }
+ console.log(getHighestSalary(company));
