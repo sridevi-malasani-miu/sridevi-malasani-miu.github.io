689600e4f263615da56fcf090b301fb3ae5fdaac
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2023-04-21 21:24:12 -0500

    Update tree.js

--- cs315/recursion/tree.js
+++ cs315/recursion/tree.js
@@ -48,3 +48,68 @@ function collectNodes(root){
 }
 
 console.log(collectNodes(abe));
+
+//4
+function ListNode(value){
+    this.value =value;
+    this.next =null;
+}
+const abeList = new ListNode('Abe');
+const homerList = new ListNode('Homer');
+const bartList = new ListNode('Bart');
+const lisaList = new ListNode('Lisa');
+const maggieList = new ListNode('Maggie');
+abeList.next=homerList;
+homerList.next=bartList;
+bartList.next=lisaList;
+lisaList.next=maggieList;
+
+//addNode
+function addNode(root,nodeName,newNode){
+    if(root.value==nodeName)
+    {
+        root.descendents.push(new TreeNode(newNode));
+        return;
+    }
+    for(let child of root.descendents){
+        addNode(child,nodeName,newNode)
+    }
+    return;
+}
+
+// addNode(abe,'Lisa','newNode');
+// console.log(JSON.stringify(abe))
+
+//deleteNode
+function deleteNode(root,nodeName){
+    for(let i=0;i<root.descendents.length;i++){
+        if(root.descendents[i].value==nodeName)
+        {
+            root.descendents.splice(i,1);
+            break;
+        }else
+            deleteNode(root.descendents[i],nodeName)
+    }
+    return;
+}
+
+// deleteNode(abe,'Lisa','newNode');
+// console.log(JSON.stringify(abe))
+
+//6
+function treeModifier(root,modifierFn){
+    modifierFn(root);
+    for(let child of root.descendents){
+        treeModifier(child,modifierFn);
+    }
+    return;
+}
+//all caps
+// treeModifier(abe,function (node){node.value=node.value.toUpperCase();})
+// console.log(abe)
+//add stars
+// treeModifier(abe,function (node){node.value='***'+node.value+'***';})
+// console.log(abe)
+//reverseNode
+treeModifier(abe,function (node){node.value =node.value.split("").reverse().join("");})
+console.log(abe)
