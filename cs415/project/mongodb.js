53863155ca5147c92e658378b21c12e647cf3b9d
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2023-06-18 13:23:37 -0500

    Update mongodb.js

--- cs415/project/mongodb.js
+++ cs415/project/mongodb.js
@@ -1,8 +1,24 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+function insertDocument(myDb, col, data) {
+  MongoClient.connect(url, function (err, db) {
+    var dbo = db.db(myDb);
+    dbo.collection(col).insertOne(data, function (err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+}
+insertDocument("myDatabase","customers",{ name: "Company Inc", address: "Highway 37" });
+var mongo = {};
+mongo.getCustomers = async function(query){
+  var db = await MongoClient.connect(url);
+  var dbo = db.db("myDatabase");
+  var result = await dbo.collection("customers").findOne(query);
+  console.log("herere",result)
+  return result;
+}
+
+module.exports = mongo;
