c7c2f1fccb06de2b985f1f337a4c97ec596ba5c4
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2023-06-19 10:38:56 -0500

    Update expressServer.js

--- cs415/project/expressServer.js
+++ cs415/project/expressServer.js
@@ -4,46 +4,40 @@ var sql = require('./mySqlConnection');
 var mongo = require('./mongodb');
 
 app.use(express.static('public'));
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "dfdf.html" );
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/myFile', function (req, res) {
-    res.sendFile( __dirname + "//public/" + "dfdfddfinventory.html" );
- });
 
- app.get('sql/users',function(req,res){
+app.get('/mongo/save', function (req, res) {
+   res.sendFile( __dirname + "/public/" + "index.html" );
+});
+
+app.get('/mongo/sqlUser', function (req, res) {
+   res.sendFile( __dirname + "/public/" + "user.html" );
+});
+
+ app.get('/mongo/customers', async (req,res)=>{
+   var customers = await mongo.getCustomers({});
+   res.end(JSON.stringify(customers,null,'\t'));
+ })
+
+app.post('/mongo/save', function (req, res) {
+   // Prepare output in JSON format
+   let response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name,
+   };
+   mongo.saveCustomer(response);
+   res.redirect("/mongo/customers");
+});
+
+
+app.get('sql/users',function(req,res){
    sql.selectQuery("select * from amazon.Category",function(err,out){
       console.log(out);
       res.end(JSON.stringify(out,null,'\t'));
    });
  })
-
- app.get('/mongodb/customers', async (req,res)=>{
-   var customers = await mongo.getCustomers({});
-   console.log(customers);
-   res.end(JSON.stringify(customers,null,'\t'));
- })
-
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.query.first_name,
-      last_name:req.query.last_name,
-      first_name1:req.query.first_name,
-      last_name1:req.query.last_name,
-      first_name2:req.query.first_name,
-      last_name2:req.query.last_name,
-      first_name3:req.query.first_name,
-      last_name3:req.query.last_name,
-      first_name3:req.query.first_name,
-      last_name5:{hello:'hi'},
-      first_name:req.query.first_name,
-      last_name:req.query.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response,null,'\t'));
-});
 
 var server = app.listen(8080, function () {
    var host = server.address().address
