c015353060df69576173498194f83c4a0676f08d
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2023-06-19 10:39:42 -0500

    Update mongodb.js

--- cs415/project/mongodb.js
+++ cs415/project/mongodb.js
@@ -1,24 +1,26 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017";
 
-function insertDocument(myDb, col, data) {
-  MongoClient.connect(url, function (err, db) {
-    var dbo = db.db(myDb);
-    dbo.collection(col).insertOne(data, function (err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-    });
-  });
-}
-insertDocument("myDatabase","customers",{ name: "Company Inc", address: "Highway 37" });
 var mongo = {};
 mongo.getCustomers = async function(query){
   var db = await MongoClient.connect(url);
   var dbo = db.db("myDatabase");
-  var result = await dbo.collection("customers").findOne(query);
+  var result = await dbo.collection("customers").find(query).toArray();
   console.log("herere",result)
   return result;
 }
 
+mongo.saveCustomer = async function(data){
+  MongoClient.connect(url, function (err, db) {
+    var dbo = db.db("myDatabase");
+    dbo.collection("customers").insertOne(data, function (err, res) {
+      if (err) throw err;
+      console.log("document inserted",data);
+      db.close();
+    });
+  });
+}
+
+// mongo.saveCustomer({id:1,name:'ram'})
+mongo.getCustomers({})
 module.exports = mongo;
