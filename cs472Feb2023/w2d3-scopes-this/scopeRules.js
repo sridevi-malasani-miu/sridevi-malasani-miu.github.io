0de7cf18330fbad8a01e977712109ca9dd4bef5e
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2023-08-05 14:28:10 -0500

    Update scopeRules.js

--- cs472Feb2023/w2d3-scopes-this/scopeRules.js
+++ cs472Feb2023/w2d3-scopes-this/scopeRules.js
@@ -1,40 +1,40 @@
-//Read value from global scope, reassign the value inside function changes the global scope variable.
+//Function doesnot find variable, 
+//it looks for it in nearest scope
 // let x = 1;
 // function a(){
-//     var x=2;
-//     console.log(x);//2
-//     let x = 3;
-//     console.log(x);//2
+//     function b(){
+//         console.log(x);
+//     }
 // }
 // a();
-// console.log(x);//1
+// console.log(x);
 
 //local variable x is changed, doesnt affect the global variable
 // let x = 1;
 // function a(){
 //     let x=2;
-//     console.log(x);//2
+//     console.log(x);
 // }
 // a();
-// console.log(x);//1
+// console.log(x);
 
 //Declare new variable after changing the global variable, local variable is created, global
 //variable remains unchanged. 
 // let x = 1;
 // function a(){
-//     console.log(x);//undefined
+//     console.log(x);
 //     x=2;
-//     console.log(x);//2
+//     console.log(x);
 //     var x = 4;
 // }
 // a();
-// console.log(x);//1 
+// console.log(x);
 
 //Object pass by reference
 // let x = {a:1};
 // function a(x){
 //     x.a = 4;
-//     console.log(x);//{a:4}
+//     console.log(x);
 // }
 // a(x);
 // console.log(x);
@@ -44,10 +44,10 @@
 // let x = 1;
 // function a(x){
 //     x = 4;
-//     console.log(x);//4
+//     console.log(x);
 // }
 // a(x);
-// console.log(x);//
+// console.log(x);
 
 //Object reassigned inside function, doesnt change the outside object
 // let x = {a:1};
@@ -57,3 +57,11 @@
 // }
 // a(x);
 // console.log(x);
+
+//Function called with x before it is changed will have the previous value.
+let x = 1;
+function a(x) {
+    return x;
+}
+console.log(a(x))
+x = 5
