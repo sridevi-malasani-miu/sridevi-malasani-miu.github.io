84bb58b8aa0ccdbeaecc7d1566aab4f6b2cf1316
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2023-11-15 10:17:16 -0600

    Update ejsAPI.js

--- cs472/w3d3ejsCookie/ejsAPI.js
+++ cs472/w3d3ejsCookie/ejsAPI.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cookieParse = require("cookie-parser");
+
 var path = require("path");
 var ejs = require("ejs");
 
@@ -8,7 +8,7 @@ app.listen(80, () => {
     console.log('Your Server is running on 3000');
 })
 
-app.use(cookieParse());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
