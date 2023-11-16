1b63d65beb1d1c7471a33774c23365b89e626a8e
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2023-11-16 11:53:53 -0600

    Update ajax.js

--- cs472Feb2023/w3d2/ajax.js
+++ cs472Feb2023/w3d2/ajax.js
@@ -4,7 +4,6 @@ var ejs = require("ejs");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 
-
 app.listen(80, function () {
     console.log(" Your server started listening")
 })
@@ -14,14 +13,32 @@ app.use(cookieParser("salt for cookie"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//EJS Engine
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
 app.get("/form", function (req, res, next) {
-    res.sendFile(path.join(__dirname,'views','index.html'))
+    res.render('index',{data:req.cookies.myValue});
 })
+
+app.post("/save", function (req, res, next) {
+   let submittedValue = req.body.textField;
+   res.cookie('myValue',submittedValue);
+   res.redirect('/form');
+})
+
 //Toggle color by reading from cookie
 app.get('/ajaxToggleColor',function(req,res){
-    let color = req.cookies.color?req.cookies.color:'green';
+    // let color = req.cookies.color?req.cookies.color:'green';
+    // if(req.cookies.color){
+    //     color = (req.cookies.color=='green')?'red':'green';
+    // }
+    console.log(req.query);
+    let color;
     if(req.cookies.color){
         color = (req.cookies.color=='green')?'red':'green';
+    }else{
+        color = 'green';
     }
     res.cookie('color',color)
     res.send({color:color})
@@ -36,7 +53,7 @@ app.get('/ajaxError',function(req,res){
     throw new Error("Ajax error")
 })
 // Parameterized request
-app.get('/ajax/:id',function(req,res){
+app.get('/ajax/:id/:name',function(req,res){
     console.log(" ajax:id",req.params)
     res.send({color:'pink'})
 })
