7112c5ad8b6802e7c8fbce57a891d6fe36a2482f
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2024-11-25 13:37:09 -0600

    Create git.js

---
+++ cs425/script/mcq/git.js
@@ -0,0 +1,102 @@
+const questionsArray = [
+    {
+        question: 'Which command is used to initialize a new Git repository?',
+        choices: ['git clone', 'git init', 'git commit', 'git add'],
+        answer: ['git init']
+    },
+    {
+        question: 'Which commands are used to stage files in Git?',
+        choices: ['git add', 'git status', 'git commit', 'git diff'],
+        answer: ['git add']
+    },
+    {
+        question: 'Which of the following commands fetches changes from a remote repository?',
+        choices: ['git fetch', 'git pull', 'git clone', 'git init'],
+        answer: ['git fetch', 'git pull']
+    },
+    {
+        question: 'Which of the following commands are used to view commit history?',
+        choices: ['git log', 'git commit', 'git status', 'git log --oneline'],
+        answer: ['git log', 'git log --oneline']
+    },
+    {
+        question: 'Which commands are used to push changes to a remote repository?',
+        choices: ['git pull', 'git push', 'git merge', 'git add'],
+        answer: ['git push']
+    },
+    {
+        question: 'Which of the following commands merge branches in Git?',
+        choices: ['git merge', 'git branch', 'git add', 'git commit'],
+        answer: ['git merge']
+    },
+    {
+        question: 'Which command creates a local copy of a remote repository?',
+        choices: ['git clone', 'git fetch', 'git init', 'git log'],
+        answer: ['git clone']
+    },
+    {
+        question: 'Which command removes staged changes before committing?',
+        choices: ['git reset', 'git log', 'git checkout', 'git merge'],
+        answer: ['git reset']
+    },
+    {
+        question: 'Which of the following commands will show changes between the working directory and staging area?',
+        choices: ['git diff', 'git add', 'git status', 'git log'],
+        answer: ['git diff']
+    },
+    {
+        question: 'Which commands allow you to switch between branches?',
+        choices: ['git checkout', 'git merge', 'git branch', 'git add'],
+        answer: ['git checkout']
+    },
+    {
+        question: 'Which commands are used to create and list branches in Git?',
+        choices: ['git branch', 'git merge', 'git checkout', 'git init'],
+        answer: ['git branch']
+    },
+    {
+        question: 'Which commands show the current status of the repository?',
+        choices: ['git status', 'git log', 'git diff', 'git add'],
+        answer: ['git status']
+    },
+    {
+        question: 'Which commands allow you to undo a commit?',
+        choices: ['git revert', 'git reset', 'git log', 'git pull'],
+        answer: ['git revert', 'git reset']
+    },
+    {
+        question: 'Which command allows you to update the most recent commit?',
+        choices: ['git commit --amend', 'git log', 'git reset', 'git status'],
+        answer: ['git commit --amend']
+    },
+    {
+        question: 'Which of the following commands show differences in commits or files?',
+        choices: ['git diff', 'git log --oneline', 'git status', 'git merge'],
+        answer: ['git diff']
+    },
+    {
+        question: 'Which of the following commands show a graphical representation of commit history?',
+        choices: ['git log --graph', 'git log', 'git fetch', 'git checkout'],
+        answer: ['git log --graph']
+    },
+    {
+        question: 'Which commands create tags in a Git repository?',
+        choices: ['git tag -a', 'git checkout', 'git commit', 'git push'],
+        answer: ['git tag -a']
+    },
+    {
+        question: 'Which of the following commands display the configuration information of Git?',
+        choices: ['git config --list', 'git init', 'git config user.name', 'git push'],
+        answer: ['git config --list', 'git config user.name']
+    },
+    {
+        question: 'Which commands are used to fetch changes from a remote repository but not merge them?',
+        choices: ['git fetch', 'git pull', 'git log', 'git clone'],
+        answer: ['git fetch']
+    },
+    {
+        question: 'Which of the following commands display all the commits in one line format?',
+        choices: ['git log --oneline', 'git status', 'git log', 'git branch'],
+        answer: ['git log --oneline']
+    }
+];
