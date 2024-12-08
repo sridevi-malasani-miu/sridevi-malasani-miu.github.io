67ee0a64b1f8e935497069cca62c0cfc9f81c820
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2024-12-08 16:50:02 -0600

    Update ProductRepository.java

--- cs425/jpa/customerOrders/ProductRepository.java
+++ cs425/jpa/customerOrders/ProductRepository.java
@@ -10,4 +10,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
            "FROM Product p JOIN OrderDetail od ON p.id = od.product.id " +
            "GROUP BY p.productName ORDER BY totalQuantity DESC")
     List<Object[]> findMostPopularProduct();
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Product p SET p.stock = p.stock - :quantity WHERE p.id = :productId AND p.stock >= :quantity")
+    int updateStock(Long productId, int quantity);
+
 }
