76bc9fb3d652796020df487f95890d4e0bce0aad
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2024-12-08 16:44:50 -0600

    Create Product.java

---
+++ cs425/jpa/customerOrders/Product.java
@@ -0,0 +1,18 @@
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+}
