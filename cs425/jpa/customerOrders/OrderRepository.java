35b4e3014309e73d4d3fe9e0f4a0f6fba453f6e6
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2024-12-08 16:48:57 -0600

    Create OrderRepository.java

---
+++ cs425/jpa/customerOrders/OrderRepository.java
@@ -0,0 +1,13 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT c.name AS customerName, COUNT(o.id) AS totalOrders " +
+           "FROM Order o " +
+           "JOIN o.customer c " +
+           "GROUP BY c.name")
+    List<Object[]> countTotalOrdersPerCustomer();
+}
