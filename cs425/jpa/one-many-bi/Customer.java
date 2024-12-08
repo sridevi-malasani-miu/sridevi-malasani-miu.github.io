ed89fd46ee090a0079d5ef528681820e84b18fda
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2024-12-08 12:53:06 -0600

    Create Customer.java

---
+++ cs425/jpa/one-many-bi/Customer.java
@@ -0,0 +1,19 @@
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+}
