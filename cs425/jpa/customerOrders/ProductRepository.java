1454729e39fb108b576d54a173f266e194ab868c
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2024-12-08 16:43:54 -0600

    Create ProductRepository.java

---
+++ cs425/jpa/customerOrders/ProductRepository.java
@@ -0,0 +1,13 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p LEFT JOIN OrderDetail od ON p.id = od.product.id WHERE od.product.id IS NULL")
+    List<Product> findAllUnsoldProducts();
+
+    @Query("SELECT p.productName, SUM(od.quantity) AS totalQuantity " +
+           "FROM Product p JOIN OrderDetail od ON p.id = od.product.id " +
+           "GROUP BY p.productName ORDER BY totalQuantity DESC")
+    List<Object[]> findMostPopularProduct();
+}
