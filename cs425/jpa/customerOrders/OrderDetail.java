6f1608f2c4f26c8b196dd660ad994dc8e89ba8ce
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2024-12-08 16:45:32 -0600

    Create OrderDetail.java

---
+++ cs425/jpa/customerOrders/OrderDetail.java
@@ -0,0 +1,23 @@
+import jakarta.persistence.*;
+
+@Entity
+public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double totalPrice;
+
+}
