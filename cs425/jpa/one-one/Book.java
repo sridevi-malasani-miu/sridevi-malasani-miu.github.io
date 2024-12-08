66da3f405667d1faab12a50b5aa60c8a2e8c703b
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2024-12-08 12:45:49 -0600

    Create Book.java

---
+++ cs425/jpa/one-one/Book.java
@@ -0,0 +1,18 @@
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+}
