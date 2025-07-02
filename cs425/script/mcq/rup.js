64f4ba4a7a53b32275194df195c048624ba9ee93
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2025-07-02 13:09:26 +0530

    Update rup.js

--- cs425/script/mcq/rup.js
+++ cs425/script/mcq/rup.js
@@ -41,64 +41,8 @@ questionsArray = [
             'Architect'
         ]
     },
-    {
-        question: 'Which of these features are supported by the Unified Modeling Language (UML)?',
-        choices: [
-            'Abstraction',
-            'Decomposition',
-            'Zoom-in and zoom-out',
-            'Code compilation'
-        ],
-        answer: [
-            'Abstraction',
-            'Decomposition',
-            'Zoom-in and zoom-out'
-        ]
-    },
-    {
-        question: 'Which types of diagrams are part of UML?',
-        choices: [
-            'Class diagrams',
-            'Use-case diagrams',
-            'Sequence diagrams',
-            'Flowcharts'
-        ],
-        answer: [
-            'Class diagrams',
-            'Use-case diagrams',
-            'Sequence diagrams'
-        ]
-    },
-    
-    {
-        question: 'What are the core characteristics of UML?',
-        choices: [
-            'A graphical language for modeling',
-            'Focuses on object-oriented systems',
-            'Allows visualization of software designs',
-            'Not suitable for software development'
-        ],
-        answer: [
-            'A graphical language for modeling',
-            'Focuses on object-oriented systems',
-            'Allows visualization of software designs'
-        ]
-    },
-    
-    {
-        question: 'What are the goals of UML diagrams?',
-        choices: [
-            'Provide different views of the same model',
-            'Simplify communication',
-            'Increase system abstraction',
-            'Support hardware debugging'
-        ],
-        answer: [
-            'Provide different views of the same model',
-            'Simplify communication',
-            'Increase system abstraction'
-        ]
-    },
+  
+   
     {
         question: 'Which methodologies are software development methods?',
         choices: [
@@ -129,20 +73,6 @@ questionsArray = [
     },
    
     {
-        question: 'Which of the following describe UML diagrams?',
-        choices: [
-            'Graphical representations',
-            'Support software design',
-            'Primarily text-based',
-            'Allow communication between stakeholders'
-        ],
-        answer: [
-            'Graphical representations',
-            'Support software design',
-            'Allow communication between stakeholders'
-        ]
-    },
-    {
         question: 'Which steps are emphasized in RUP iterations?',
         choices: [
             'Requirements gathering',
@@ -156,20 +86,7 @@ questionsArray = [
             'Testing and evaluation'
         ]
     },
-    {
-        question: 'What are some reasons to use UML?',
-        choices: [
-            'To visualize complex systems',
-            'To provide standardization',
-            'To avoid abstraction',
-            'To enable tool support'
-        ],
-        answer: [
-            'To visualize complex systems',
-            'To provide standardization',
-            'To enable tool support'
-        ]
-    },
+   
     {
         question: 'What is a common limitation of the Waterfall method compared to RUP?',
         choices: [
@@ -184,20 +101,7 @@ questionsArray = [
             'Inflexible to changes'
         ]
     },
-    {
-        question: 'What type of modeling does UML support?',
-        choices: [
-            'Object-oriented',
-            'Behavioral',
-            'Structural',
-            'Functional programming'
-        ],
-        answer: [
-            'Object-oriented',
-            'Behavioral',
-            'Structural'
-        ]
-    },
+   
     {
         question: 'Which phases of RUP focus on coding and implementation?',
         choices: [
